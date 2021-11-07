e58cbef60e97fc94bce82dbb7c1747e6f4908fa5
maro <pmaroti@telenor.hu>
2021-11-07 13:34:08 +0100

    hilbert transform 15 tap

--- hilbert.m
+++ hilbert.m
@@ -1,8 +1,26 @@
 pkg load communications
 pkg load signal
-b=[-1/7, 0, -1/5, 0, -1/3, 0, 1, 0, 1, 0, 1/3, 0, 1/5, 0, 1/7];
 
+Fs = 1000;
+Ts = 1/Fs;
+L = 1500;
+t = (0: L-1)*Ts;
 
+fSig = 10;
+x = cos (2 * pi * fSig * t) ; 
 
+b=[-1/7, 0, -1/5, 0, -1/3, 0, -1, 0, 1, 0, 1/3, 0, 1/5, 0, 1/7];
+cntr=0;
+oi=[];
+oq=[];
+dl=zeros(15,1)'; #'
+for s = x
+    dl=shift(dl,1);
+    dl(1)=s;
+    oi=[oi,dl(7)];
+    oq=[oq,2*b*dl']; #'
+endfor
 
-
+plot(oi);
+hold on;
+plot(oq);
